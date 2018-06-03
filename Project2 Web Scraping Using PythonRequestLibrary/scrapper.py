7753065870867d137dd8c3f944fdfe17e14f47eb
prateek951 <prince.cenation@gmail.com>
2018-06-03 20:35:15 +0530

    getting error at line26

--- Project2 Web Scraping Using PythonRequestLibrary/scrapper.py
+++ Project2 Web Scraping Using PythonRequestLibrary/scrapper.py
@@ -1,7 +1,7 @@
 import requests 
 from bs4 import BeautifulSoup
 
-WEB_URL = 'https://www.yellowpages.com/los-angeles-ca/coffee?g=los%20angeles%2C%20ca&q=coffee'
+WEB_URL = 'http://www.yellowpages.com/los-angeles-ca/coffee?g=los%20angeles%2C%20ca&q=coffee'
 
 # Reach out to the URL to grab the data
 r = requests.get(WEB_URL)
@@ -13,6 +13,14 @@ soup = BeautifulSoup(r.content)
 
 links = soup.find_all("a")
 print(links)
+#list of all the links
 
 for link in links:
     print("<a href='%s'>%s</a>" %(link.get("href"),link.text))
+
+g_data = soup.find_all("div",{"class":"info"})
+# print(g_data)
+
+for item in g_data:
+    print(item.contents[0].find_all("a",{"class":"business-name"})[0].text)
+    print(item.contents[1].find_all("p",{"class":"adr"}))
