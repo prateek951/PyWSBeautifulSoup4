4363758bcdd5ccd803015a3c0bfa1dac49dfb430
prateek951 <prince.cenation@gmail.com>
2018-06-03 21:38:30 +0530

    added the scrapping code

--- Project2 Web Scraping Using PythonRequestLibrary/scrapper.py
+++ Project2 Web Scraping Using PythonRequestLibrary/scrapper.py
@@ -22,8 +22,13 @@ g_data = soup.find_all("div",{"class":"info"})
 # print(g_data)
 
 for item in g_data:
-    print(item.contents[0].find_all("a",{"class":"business-name"})[0].text)
+    business_name = item.contents[0].find_all("a",{"class":"business-name"})[0].text
     try:
         print(item.contents[1].find_all("p", {"class": "adr"})[0].text)
+        print(item.contents[1].find_all("span",{"itemprop":"street-address"})[0].text)
+        print(item.contents[1].find_all("span",{"itemprop":"addressLocality"})[0].text.replace(',',''))
+        print(item.contents[1].find_all("span",{"itemprop":"addressRegion"})[0].text)
+        print(item.contents[1].find_all("span",{"itemprop":"telephone"})[0].text)
     except:
         pass
+   
